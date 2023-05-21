d0e699327279d5d5b56ab2fb20be3bff0152e156
VibavariG <vibu2301@gmail.com>
2023-05-21 15:38:59 -0700

    maint: Add dynamic hello message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
 const App = () => {
+  const name = null
+  //const isNameShowing = false
   return (
     <div className="App"> 
-        <h1>Hello, React!</h1>
+        <h1>Hello,</h1>
+        {name ? (
+          <>
+          <h1>{name}</h1>
+          </>
+        ): (
+          <>
+          <h1>test</h1>
+          <h2>someone</h2>
+          </>
+        )}
     </div>
   );
 }
