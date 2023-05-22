92f002828e99e06e60f302ad79f8b1b4b6e3ade0
Vibavari Gurunathan <42400331+VibavariG@users.noreply.github.com>
2023-05-22 16:01:31 -0700

    Create MovieLand app (#1)
    * maint: Remove comments
    
    * feat: Change display to Hello, React
    
    * feat: Add dynamic hello message
    
    * feat: Create component Person and use multiple times
    
    * feat: Use props to pass dynamic data to components
    
    * feat: Pass dynamic data to component
    
    * feat: Counter - Add status, hook, event
    
    * feat: Set counter to static value
    
    * feat: Add dynamic counters
    
    * feat: Change state variable manually
    
    * feat: Display alert on every counter change
    
    * feat: Add boilerplate, api for movie project
    
    * feat: Add css code for movieland
    
    * feat: Add Search bar and icon
    
    * feat: Add one movie card with static movie details
    
    * feat: Create movie component, Display multiple movies
    
    * feat: Complete Search bar and button functionality
    
    * maint: Fix formatting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = "http://www.omdbapi.com?apikey=f39f7cd3"
+
+const App = () => {
+    //asynchronous data mean its gonna take some time to fetch these movies
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [movieSearched, setMovieSearched] = useState(false);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+        if (!movieSearched) {
+            setMovieSearched(true);
+        }
+    }
+
+    const displayMovies = (movies) => {
+        return(
+            movies?.length > 0 ?(
+                <div className='container'>
+                    {movies.map((movie)=> (
+                        <MovieCard movie = {movie}/>
+                    ))}
+                </div>
+            ) : (
+                <div className = 'empty'>
+                    <h2>No movies found</h2>
+                </div>
+            )
+        );
+    }
+
+    useEffect(() => {
+    }, [])
+
+    return (
+        <div className='app'>
+            <h1>MovieLand</h1>
+            <div className='search'>
+                <input
+                  placeholder='Search for Movies'
+                  value = {searchTerm}
+                  onChange = {(e)=>setSearchTerm(e.target.value)}
+                />
+                <img
+                  src = {SearchIcon}
+                  alt='search'
+                  onClick = {()=> searchMovies(searchTerm)}
+                />
+            </div>
+            {movieSearched ? displayMovies(movies) : null}
+        </div>
+    )
 }
 
 export default App;
