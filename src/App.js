c93583a26ca84a8a9d560091b67c9c122a562696
VibavariG <vibu2301@gmail.com>
2023-05-22 01:24:21 -0700

    feat: Add boilerplate, api for movie project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import './App.css';
-import { useState, useEffect } from 'react'
+import {useEffect} from 'react'
+
+const API_URL = "http://www.omdbapi.com?apikey=f39f7cd3"
 
 const App = () => {
+    //asynchronous data mean its gonna take some time to fetch these movies
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
 
-  const [counter, setCounter] = useState(0);  //array destructuring, calling something as a function and it starts with 'use', we call that a hook
-  //name of that state, setter function [second var same as first var, with 'set' in front of it]
-  //inside the useState(), give initial value, here, 0.
-
-  useEffect(()=>{       //hapens as soon as the App component renders i.e every time I make a state change
-    alert(counter)
-    //react state should only be changed using its own setter, here, setCounter
-    //setCounter(100);
-  }, [counter])
-
-  //[] means it's only gonna happen at the initial load of the component
-  //[counter] means it happens everytime the variable inside [] changes
-
-  return (
-    <div className="App"> 
-      <button onClick={() => setCounter((prevCount) => prevCount-1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount+1)}>+</button>
-    </div>
-  );
+        console.log(data.Search);
+    }
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, [])
+    return (
+        <h1>App</h1>
+    )
 }
 
 export default App;
