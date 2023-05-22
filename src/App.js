d9908c066fed729b29f610dc3ff2905bfdf3dcfa
VibavariG <vibu2301@gmail.com>
2023-05-22 13:50:53 -0700

    feat: Add one movie card with static movie details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import SearchIcon from './search.svg';
 
 const API_URL = "http://www.omdbapi.com?apikey=f39f7cd3"
 
+const movie1 = {
+    "Title": "Spiderman in Cannes",
+    "Year": "2016",
+    "imdbID": "tt5978586",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
+}
+
 const App = () => {
     //asynchronous data mean its gonna take some time to fetch these movies
     const searchMovies = async (title) => {
@@ -27,7 +35,24 @@ const App = () => {
                 />
                 <img
                   src = {SearchIcon}
+                  alt='search'
+                  onClick = {()=>{}}
                 />
+            </div>
+
+            <div className='container'>
+                <div className='movie'>
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie1.Poster!= 'N/A' ? movie1.Poster: 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                    </div>
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
             </div>
         </div>
     )
