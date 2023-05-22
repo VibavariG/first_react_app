e4ecdaf3a146743d59cccd81acad15a2258c9555
VibavariG <vibu2301@gmail.com>
2023-05-22 12:28:16 -0700

    feat: Add Search bar and icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import {useEffect} from 'react'
+import {useEffect} from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
 
 const API_URL = "http://www.omdbapi.com?apikey=f39f7cd3"
 
@@ -7,14 +9,27 @@ const App = () => {
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
         const data = await response.json();
-
         console.log(data.Search);
     }
+
     useEffect(() => {
         searchMovies('Spiderman');
     }, [])
+
     return (
-        <h1>App</h1>
+        <div className='app'>
+            <h1>MovieLand</h1>
+            <div className='search'>
+                <input
+                  placeholder='Search for Movies'
+                  value = 'Superman'
+                  onChange = {()=>{}}
+                />
+                <img
+                  src = {SearchIcon}
+                />
+            </div>
+        </div>
     )
 }
 
