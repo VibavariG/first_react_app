dbf4d883103c33ccff28e104c495274f7910a927
VibavariG <vibu2301@gmail.com>
2023-05-22 15:55:58 -0700

    maint: Fix formatting

--- src/App.js
+++ src/App.js
@@ -5,27 +5,38 @@ import MovieCard from './MovieCard'
 
 const API_URL = "http://www.omdbapi.com?apikey=f39f7cd3"
 
-const movie1 = {
-    "Title": "Spiderman in Cannes",
-    "Year": "2016",
-    "imdbID": "tt5978586",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
-}
-
 const App = () => {
     //asynchronous data mean its gonna take some time to fetch these movies
     const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('')
+    const [searchTerm, setSearchTerm] = useState('');
+    const [movieSearched, setMovieSearched] = useState(false);
 
     const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`)
+        const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
         setMovies(data.Search);
+        if (!movieSearched) {
+            setMovieSearched(true);
+        }
+    }
+
+    const displayMovies = (movies) => {
+        return(
+            movies?.length > 0 ?(
+                <div className='container'>
+                    {movies.map((movie)=> (
+                        <MovieCard movie = {movie}/>
+                    ))}
+                </div>
+            ) : (
+                <div className = 'empty'>
+                    <h2>No movies found</h2>
+                </div>
+            )
+        );
     }
 
     useEffect(() => {
-        searchMovies('Spiderman');
     }, [])
 
     return (
@@ -43,24 +54,7 @@ const App = () => {
                   onClick = {()=> searchMovies(searchTerm)}
                 />
             </div>
-
-            {
-                movies?.length > 0
-                ?(
-                    <div className='container'>
-                        {movies.map((movie)=> (
-                            <MovieCard movie = {movie}/>
-                        ))}
-                        
-                    </div>
-                ) : (
-                    <div className = 'empty'>
-                        <h2>No movies found</h2>
-                    </div>
-                )
-            }
-
-            
+            {movieSearched ? displayMovies(movies) : null}
         </div>
     )
 }
