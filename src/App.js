d010393ba63d32b3a9293f799a7b4f120bd2b16a
VibavariG <vibu2301@gmail.com>
2023-05-21 17:54:32 -0700

    maint: Pass dynamic data to component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import './App.css';
 const Person = (props) => {
   return (
     <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
     </>
   )
 }
 const App = () => {
   return (
     <div className="App"> 
-        <Person name={'Vibavari'} lastName={'Gurunathan'} age={30}/>
-        <Person/>
-        <Person/>
-        <Person/>
-        <Person/>
+      <Person name='Vibavari' lastName='Gurunathan' age={21}/>
+      <Person name='Twinkle' lastName='Madan' age= {23}/>
     </div>
   );
 }
